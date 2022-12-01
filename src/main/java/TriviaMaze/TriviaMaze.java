82dee1a3058f3f9a54abb334cdbde93e612615c8
angelynnapyeatt <angielynnapyeatt@gmail.com>
2022-11-30 18:31:35 -0800

    Ensured CheckStyle was satisfied

--- src/main/java/TriviaMaze/TriviaMaze.java
+++ src/main/java/TriviaMaze/TriviaMaze.java
@@ -1,29 +1,47 @@
+/**
+ * Attack of Trivia: TriviaMaze.java
+ * 12/15/2022
+ */
 package TriviaMaze;
 
-
+import java.io.Serializable;
 import java.util.Scanner;
 
 //I was thinking we could connect our SQLite DataBase to TriviaMaze
 //So that when player is trying to move through a door I can ask a question
 //And depending on how they answer I'll either lock or unlock the door
 
+/**
+ * TriviaMaze takes in player input and runs the game
+ *
+ * @author Amtoj Kaur, Angelynna Pyeatt, Leyla Ahmed
+ * @version Autumn 2022
+ */
+
 public class TriviaMaze {
+    /** holds maze.*/
     private final Maze myMaze;
+    /** holds current game.*/
     private final Game myGame = new Game();
+    /** holds Scanner for game.*/
     private final Scanner scanner = new Scanner(System.in);
 
-    //This constructor's parameters takes in the Maze
-    //and if we decide the SQLight database as well.
-    protected TriviaMaze(Maze theMaze) {
+    /**
+     * Constructor starts the game and initializes the maze.
+     *
+     * @param theMaze
+     */
+    protected TriviaMaze(final Maze theMaze) {
         startGame();
         this.myMaze = theMaze;
 
     }
-    //print maze out and the player movement instructions
-    //depending on player input create a newGame,saveGame,or exitGame.
+
+    /**
+     * print maze out and the player movement instructions
+     * depending on player input create a newGame,saveGame,or exitGame.
+     */
     private void newGame() {
-
-
         System.out.print(this.myMaze);
         myGame.playerMovement();
         myGame.playerMenu();
@@ -31,10 +49,14 @@ public class TriviaMaze {
 
     }
 
-    private void startGame(){
+    /**
+     * Takes player input and determines how the player will play.
+     */
+    private void startGame() {
         myGame.gameMenu();
         int playerInput = Integer.parseInt(scanner.nextLine());
-        if(playerInput == 1){
+
+        if (playerInput == 1) {
             System.out.println("Started a new game.\n");
             newGame();
         } else if (playerInput == 2) {
@@ -42,19 +64,28 @@ public class TriviaMaze {
             //load game
         } else if (playerInput == 3) {
             System.out.println("Thank you for playing");
-        }else {
+        } else {
             System.out.println("Invalid input");
         }
     }
 
-    //Get the players input
+    /**
+     * gets player input.
+     *
+     * @return input
+     */
     private String getPlayerInput() {
         String input;
         input = scanner.nextLine().toLowerCase();
         return input;
     }
-    //Depending on player input, move the player.
-    private void playerInput(String theInput) {
+
+    /**
+     * moves player depending on input.
+     *
+     * @param theInput
+     */
+    private void playerInput(final String theInput) {
         switch (theInput) {
             case "w" -> movePlayerNorth();
             case "a" -> movePlayerWest();
@@ -65,58 +96,79 @@ public class TriviaMaze {
             case "e" -> System.out.println("Thank you for playing");
         }
     }
-    private void setPlayerLocation(){
+
+    /**
+     * sets player location to default location: (0, 0).
+     */
+    private void setPlayerLocation() {
         myMaze.setLocation(0, 0);
     }
 
+    /**
+     * Moves player North.
+     */
     private void movePlayerNorth() {
-        if (myMaze.isLocked('N')) {// if the door in the North direction is not locked
+        if (myMaze.isLocked('N')) {
             System.out.println("You cannot move North");
-        } else if (askQuestion()) {//if the door is locked ask a question,correct answer unlock door
+        } else if (askQuestion()) {
             System.out.println("You can continue");
             myMaze.move('N');
-        } else {//incorrect answer lock the door
+        } else {
             System.out.println("INCORRECT ANSWER, DOOR HAS BEEN LOCKED");
             myMaze.isLocked('N');
         }
     }
 
+    /**
+     * Moves player West.
+     */
     private void movePlayerWest() {
-        if (myMaze.isLocked('W')) {// if the door in the West direction is not locked
+        if (myMaze.isLocked('W')) {
             System.out.println("You cannot move West");
-        } else if (askQuestion()) {//if the door is locked ask a question,correct answer unlock door
+        } else if (askQuestion()) {
             System.out.println("You can continue");
             myMaze.move('W');
-        } else {//incorrect answer lock the door
+        } else {
             System.out.println("INCORRECT ANSWER, DOOR HAS BEEN LOCKED");
             myMaze.isLocked('W');
         }
     }
 
+    /**
+     * Moves player East.
+     */
     private void movePlayerEast() {
-        if (myMaze.isLocked('E')) {// if the door in the East direction is not locked
+        if (myMaze.isLocked('E')) {
             System.out.println("You cannot move East");
-        } else if (askQuestion()) {//if the door is locked ask a question,correct answer unlock door
+        } else if (askQuestion()) {
             System.out.println("You can continue");
             myMaze.move('E');
-        } else {//incorrect answer lock the door
+        } else {
             System.out.println("INCORRECT ANSWER, DOOR HAS BEEN LOCKED");
             myMaze.isLocked('E');
         }
     }
 
+    /**
+     * Moves player South.
+     */
     private void movePlayerSouth() {
-        if (myMaze.isLocked('S')) {// if the door in the South direction is not locked
+        if (myMaze.isLocked('S')) {
             System.out.println("You cannot move South");
-        } else if (askQuestion()) {//if the door is locked ask a question,correct answer unlock door
+        } else if (askQuestion()) {
             System.out.println("You can continue");
             myMaze.move('S');
-        } else {//incorrect answer lock the door
+        } else {
             System.out.println("INCORRECT ANSWER, DOOR HAS BEEN LOCKED");
             myMaze.isLocked('S');
         }
     }
 
+    /**
+     * Asks the player a question.
+     *
+     * @return getPlayerInput().equals("A");
+     */
     private boolean askQuestion() {
         System.out.println("Is 2 an even number");
         System.out.println("Press A: for true");
@@ -124,16 +176,20 @@ public class TriviaMaze {
         return getPlayerInput().equals("A");
     }
 
+    /**
+     * ends the game when the player has reached the end or lost.
+     *
+     * @return false
+     */
     private boolean endGame() {
-        if(myMaze.atLastRoom()){
+        if (myMaze.atLastRoom()) {
             System.out.println("You've won the game!!!");
-        } else if (myMaze.isLocked('N')&&myMaze.isLocked('S')
-                && myMaze.isLocked('E')&& myMaze.isLocked('W')) {
+        } else if (myMaze.isLocked('N') && myMaze.isLocked('S')
+                && myMaze.isLocked('E') && myMaze.isLocked('W')) {
             System.out.println("You've LOST the game");
         }
         return false;
 
     }
-
 
 }
