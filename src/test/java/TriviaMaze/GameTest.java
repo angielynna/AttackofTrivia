90b50368a617dcdca63671afc8148c4819308760
angelynnapyeatt <angielynnapyeatt@gmail.com>
2022-12-03 14:10:55 -0800

    Added GameTest.java

---
+++ src/test/java/TriviaMaze/GameTest.java
@@ -0,0 +1,16 @@
+package TriviaMaze;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void loadGame() {
+    }
+
+    @Test
+    void saveGame() {
+    }
+}
