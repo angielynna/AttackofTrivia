52fd3c5e559fcedd23515abc0d6cf6eb5c381b9f
angelynnapyeatt <angielynnapyeatt@gmail.com>
2022-12-05 19:15:58 -0800

    Troubleshooting issues:

--- src/test/java/TriviaMaze/QuestionTest.java
+++ src/test/java/TriviaMaze/QuestionTest.java
@@ -7,83 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class QuestionTest {
 
     @Test
-    void questionException() {
+    void questionExceptionConstructor() {
         assertThrows(IllegalArgumentException.class, () -> new Question(null,
-                new String[]{"1", "2", "3", "4"}, "b"));
+                "A. Hi, B. Hello, C. Bye, D. Goodbye", "B"));
     }
 
     @Test
-    void ansOptionException() {
+    void ansOptionExceptionConstructor() {
         assertThrows(IllegalArgumentException.class, () -> new Question("Have you ever...?",
-                new String[]{"1", "2"}, "b"));
+                null, "B"));
     }
 
     @Test
-    void answerChoiceException() {
+    void answerChoiceExceptionConstructor() {
         assertThrows(IllegalArgumentException.class, () -> new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, null));
-    }
-
-    @Test
-    void getQuestion() {
-        Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertEquals("Have you ever...?", q.getQuestion());
-    }
-
-    @Test
-    void getIllegalQuestion() {
-        Question q = new Question(null,
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertThrows(IllegalArgumentException.class, () -> q.getQuestion());
-    }
-
-    @Test
-    void getAnswer() {
-        Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertEquals("b", q.getAnswer());
-    }
-
-    @Test
-    void getIllegalAnswer() {
-        Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, null);
-        assertThrows(IllegalArgumentException.class, () -> q.getAnswer());
-    }
-
-    @Test
-    void getOptions() {
-        Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertEquals("1\n2\n3\n4", q.getOptions());
-    }
-
-    @Test
-    void getIllegalOptions() {
-        Question q = new Question("Have you ever...?",
-                new String[]{"1", "2"}, "b");
-        assertThrows(IllegalArgumentException.class, () -> q.getOptions());
+                "A. Hi, B. Hello, C. Bye, D. Goodbye", null));
     }
 
     @Test
     void isCorrect() {
         Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertEquals(true, q.isCorrect("b"));
+                "A. Hi, B. Hello, C. Bye, D. Goodbye", "B");
+        assertEquals(true, q.isCorrect("B"));
     }
 
     @Test
     void isNotCorrect() {
         Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertEquals(false, q.isCorrect("c"));
+                "A. Hi, B. Hello, C. Bye, D. Goodbye", "B");
+        assertEquals(false, q.isCorrect("C"));
     }
 
     @Test
     void promptQuestion() {
         Question q = new Question("Have you ever...?",
-                new String[]{"1", "2", "3", "4"}, "b");
-        assertEquals("Have you ever...?\n1\n2\n3\n4\n", q.promptQuestion());
+                "A. Hi, B. Hello, C. Bye, D. Goodbye", "B");
+        assertEquals("Have you ever...?\nA. Hi\nB. Hello\nC. Bye\nD. Goodbye\n", q.promptQuestion());
     }
 }
