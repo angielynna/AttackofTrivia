5494429a35b1af24c0a4c41e9fbf856950018399
angelynnapyeatt <angielynnapyeatt@gmail.com>
2022-12-14 12:53:13 -0800

    Checkstyle and Javadoc for Game

--- src/main/java/triviamaze/Game.java
+++ src/main/java/triviamaze/Game.java
@@ -5,13 +5,13 @@
 
 package triviamaze;
 
-import java.io.*;
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
+ * View.
  * Game displays all necessary information to the player (view), calls the
- * controller class, and saves and loads games
- * View
+ * controller class, and saves and loads games.
  *
  * @author Amtoj Kaur, Angelynna Pyeatt, Leyla Ahmed
  * @version Autumn 2022
@@ -22,14 +22,17 @@ public class Game implements Serializable {
     @Serial
     private static final long serialVersionUID = 5;
 
-
+    /**
+     * Prints welcome message.
+     */
     void welcomeMessage() {
-        System.out.println("Welcome to Attack of Trivia!\n" +
-                "Where we provide you with random trivia questions within four topics" +
-                " (which you get to choose from.)\n" +
-                "Here we will test your trivia knowledge while you try to make your way " +
-                "out of a 4 by 4 maze. Be careful you don’t get trapped! That’ll make you lose the game. \n" +
-                "Are you prepared?\n");
+        System.out.println("Welcome to Attack of Trivia!\n"
+                + "Where we provide you with random trivia questions"
+                + " within four topics (which you get to choose from.)\n"
+                + "Here we will test your trivia knowledge while you"
+                + " try to make your way out of a 4 by 4 maze. Be careful"
+                + " you don’t get trapped! That’ll make you lose the game.\n"
+                + "Are you prepared?\n");
     }
     /**
      * Displays menu for the TriviaMaze.
