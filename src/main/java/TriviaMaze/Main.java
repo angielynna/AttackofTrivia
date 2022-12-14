3a16849552ab6de9038b3353109008b787095020
angelynnapyeatt <angielynnapyeatt@gmail.com>
2022-12-13 19:34:40 -0800

    Added welcome message

--- src/main/java/TriviaMaze/Main.java
+++ src/main/java/TriviaMaze/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String [] args) throws Exception {
-
-        new TriviaMaze(new DataBank(),new Game(), new Scanner(System.in));
+        Game g = new Game();
+        g.welcomeMessage();
+        new TriviaMaze(new DataBank(), g, new Scanner(System.in));
     }
 }
