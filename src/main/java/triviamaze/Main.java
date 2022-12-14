87974896043c6dd526de5e66e946a0096809913c
angelynnapyeatt <angielynnapyeatt@gmail.com>
2022-12-14 12:47:01 -0800

    Checkstyle and Javadoc for Main

--- src/main/java/triviamaze/Main.java
+++ src/main/java/triviamaze/Main.java
@@ -1,9 +1,26 @@
+/**
+ * Attack of Trivia: Main.java
+ * 12/15/2022
+ */
 package triviamaze;
 
 import java.util.Scanner;
 
+/**
+ * Main runs the entire game.
+ *
+ * @author Amtoj Kaur, Angelynna Pyeatt, Leyla Ahmed
+ * @version Autumn 2022
+ */
+
 public class Main {
-    public static void main (String [] args) throws Exception {
+    /**
+     * Main method.
+     *
+     * @param theArgs
+     * @throws Exception
+     */
+    public static void main(final String[] theArgs) throws Exception {
         Game g = new Game();
         g.welcomeMessage();
         new TriviaMaze(new DataBank(), g, new Scanner(System.in));
